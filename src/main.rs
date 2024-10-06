e688e07752f6c16048bb0738e92432adb2a61a80
Mingwei Zhang <mingwei@mwzhang.com>
2024-10-06 14:02:40 -0700

    initial version

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use tracing::{error, info};
+
+fn main() {
+    tracing_subscriber::fmt().with_ansi(false).init();
+
+    dotenvy::dotenv().ok();
+
+    let mut commons = bgpkit_commons::BgpkitCommons::new();
+    commons.load_asinfo(true, true, true).unwrap();
+    let as_info_map = commons.asinfo_all().unwrap();
+
+    let mut writer = oneio::get_writer("asinfo.jsonl").unwrap();
+    let mut info_vec = as_info_map.values().collect::<Vec<_>>();
+    info_vec.sort_by(|a, b| a.asn.cmp(&b.asn));
+    for as_info in info_vec {
+        writeln!(writer, "{}", serde_json::to_string(&as_info).unwrap()).unwrap();
+    }
+
+    if let Ok(path) = std::env::var("ASNINFO_UPLOAD_PATH") {
+        if oneio::s3_env_check().is_err() {
+            error!("S3 environment variables not set, skipping upload");
+        } else {
+            let (bucket, key) = oneio::s3_url_parse(&path).unwrap();
+            oneio::s3_upload(&bucket, &key, "asinfo.jsonl").unwrap();
+            info!("uploaded asinfo.jsonl to {}", path);
+        }
+    }
+    drop(writer);
+}
